c68f64f0430d33e605be8818002732a29b96c0d8
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-06-24 09:11:33 -0300

    Ajuste retirada Mark@login

--- testsChrome/0-login/test_login.py
+++ testsChrome/0-login/test_login.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.jira("KAN-9")   
+
 def test_login():
     options = Options()
     options.add_argument("--headless")
